c2a2d4ac792e5f7bf75d3da6f26ab1a84ccec260
Daniel Alves <danielgsmax@gmail.com>
2023-09-09 20:34:09 +0100

    first commit - commiting all files to the repository

---
+++ js/main.js
@@ -0,0 +1,50 @@
+import { FadeIn, SlideIn } from "./transitions/transitions";
+
+// Hero Section Animations
+
+const heroText = document.getElementById('heroText')
+const heroImage = document.getElementById('heroImage')
+const mainCard = document.getElementById('mainCard')
+
+window.addEventListener('load', ()=>{
+    FadeIn(heroText)
+    SlideIn(heroImage)
+})
+
+
+// Why Us Animations
+
+// Selecciona todos los elementos que deseas animar
+
+const whyUsCards = document.querySelectorAll('.why-us--card');
+
+// Crea una instancia del Intersection Observer
+
+const observer1 = new IntersectionObserver((entries,observer)=>{
+    entries.forEach(entry =>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('card-slide-in');
+        } 
+    })
+});
+
+// Observa todos los elementos
+whyUsCards.forEach(card =>{
+    observer1.observe(card)
+})
+
+
+// Main Card animation
+
+const observer2 = new IntersectionObserver((entries,object)=>{ 
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.style.opacity = "1";
+        }
+    })
+})
+
+observer2.observe(mainCard);
+
+
+
---
+++ js/transitions/transitions.js
@@ -0,0 +1,9 @@
+const FadeIn = (element)=>{
+    element.style.opacity = 1
+}
+
+const SlideIn = (element)=>{
+    element.style.transform = "translateX(0%)"
+}
+
+export {FadeIn, SlideIn}
